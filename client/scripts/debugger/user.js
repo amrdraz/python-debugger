ac1e8c4deb8d53c2dc6d54baa40de68333bbeae9
Amr Draz <amr.m.draz@gmail.com>
2015-09-02 18:46:20 +0200

    set link to github on repo and removedshow js button keeping the function for debugging purpose hidden

--- client/scripts/debugger/user.js
+++ client/scripts/debugger/user.js
@@ -76,7 +76,9 @@
             var root = $B.py2js(src, module_name, module_name, '__builtins__')
                 //earney
             var js = root.to_js();
-            if ($B.debug > 1) { console.log(js); }
+            if ($B.debug > 1) {
+                console.log(js);
+            }
 
             if ($B.async_enabled) {
                 js = $B.execution_object.source_conversion(js);
@@ -90,8 +92,8 @@
         } catch (exc) {
             $B.leave_frame();
             $B.leave_frame();
-            if(exc.$py_error) {
-              doc("console").value += _b_.getattr(exc, 'info') + '\n' + _b_.getattr(exc, '__name__') + ": " +exc.$message + '\n';  
+            if (exc.$py_error) {
+                doc("console").value += _b_.getattr(exc, 'info') + '\n' + _b_.getattr(exc, '__name__') + ": " + exc.$message + '\n';
             } else {
                 throw exc;
             }
@@ -134,9 +136,9 @@
         doc('stop').disabled = false
         if (Debugger.is_recorded()) {
             if (Debugger.get_recorded_states().length > 0) {
-               gotoLine(Debugger.get_recorded_states()[0].next_line_no);
+                gotoLine(Debugger.get_recorded_states()[0].next_line_no);
             } else {
-               Debugger.stop_debugger();
+                Debugger.stop_debugger();
             }
         } else {
             Debugger.step_debugger()
@@ -170,18 +172,18 @@
     }
 
 
-function debug_error(err, Debugger) {
-    if (Debugger.get_recorded_states().length === 0) {
-        doc('console').value = err.data;
-        Debugger.stop_debugger();
+    function debug_error(err, Debugger) {
+        if (Debugger.get_recorded_states().length === 0) {
+            doc('console').value = err.data;
+            Debugger.stop_debugger();
+        }
     }
-}
 
     function show_js(ev) {
         var src = editor.getValue();
         doc("console").value = $B.py2js(src, '__main__', '__main__', '__builtins__').to_js();
     }
-
+    window.show_js = show_js;
 
 
     Debugger.on_debugging_started(debug_started);
@@ -195,5 +197,4 @@ function debug_error(err, Debugger) {
     doc('step').addEventListener('click', step_debugger);
     doc('back').addEventListener('click', step_back_debugger);
     doc('stop').addEventListener('click', stop_debugger);
-    doc('show_js').addEventListener('click', show_js);
 })();
